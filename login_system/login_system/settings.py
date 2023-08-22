ef0daff1b7ad788eefbc66ec9931d4be56fe1582
Luana Louza <95712511+luanalouza@users.noreply.github.com>
2023-08-22 17:32:37 -0300

    add requeriments.txt

--- login_system/login_system/settings.py
+++ login_system/login_system/settings.py
@@ -85,7 +85,7 @@ ROOT_URLCONF = 'login_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
