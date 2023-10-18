75bd08badd80022d8eaffa55105763a6fc2010fb
Luana Louza <95712511+luanalouza@users.noreply.github.com>
2023-10-18 14:44:17 -0300

    maintance

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\louza\Downloads\System_login_project\System_login_project\venv
+set VIRTUAL_ENV=C:\Users\louza\OneDrive\Área de Trabalho\System_login_Django-project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
